d1dd46845b417789d1b375b92a8f06f16173619f
Raiyan <raiyan5764@gmail.com>
2022-03-20 13:15:17 +0600

    initial setup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-class Hello {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-}
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect with mongoDb function
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
